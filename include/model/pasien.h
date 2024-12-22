67be9f2c1a3585c9b7a1d26c577a7d30cb3f2724
UrayMR <umrajsyafiq@gmail.com>
2024-12-22 12:05:47 +0700

    ci: remodified pasien.h after last commit

--- include/model/pasien.h
+++ include/model/pasien.h
@@ -19,17 +19,8 @@ void searchPasien();
 void sortPasiens();
 int isDuplicatePasienId(int id);
 
-int compareByIdAsc(Pasien a, Pasien b);
-int compareByIdDesc(Pasien a, Pasien b);
-int compareByNameAsc(Pasien a, Pasien b);
-int compareByNameDesc(Pasien a, Pasien b);
-int compareByAgeAsc(Pasien a, Pasien b);
-int compareByAgeDesc(Pasien a, Pasien b);
-int compareByRoomIdAsc(Pasien a, Pasien b);
-int compareByRoomIdDesc(Pasien a, Pasien b);
-
-void bubbleSort(Pasien arr[], int n, int (*compare)(Pasien, Pasien));
-void selectionSort(Pasien arr[], int n, int (*compare)(Pasien, Pasien));
-void insertionSort(Pasien arr[], int n, int (*compare)(Pasien, Pasien));
+void bubbleSort(Pasien arr[], int n, int choice, int ascending);
+void selectionSort(Pasien arr[], int n, int choice, int ascending);
+void insertionSort(Pasien arr[], int n, int choice, int ascending);
 
 #endif
